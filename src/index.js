515c9124cbcf8f9901438bd4041b9266320562af
dench1k <denmoskvitin@gmail.com>
2020-10-22 12:48:56 +0300

    feat: add vars

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
 module.exports = function toReadable(number) {
     const digitNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const tenDigitNames = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
+    const isSecondZero = number.toString()[1] === '0';
+    const isSecondOne = number.toString()[1] === '1';
+    const isLastZero = number.toString().endsWith('0');
+    const getTenDigit = tenDigitNames[number.toString()[0] - 2];
+    const getTenDigitSecond = digitNames[number.toString()[1]];
+    const getHundredDigit = digitNames[number.toString()[0]];
+    const getHundredDigitTens = digitNames[number.toString()[1] + number.toString()[2]];
+    const getHundredDigitTensRound = tenDigitNames[number.toString()[1] - 2];
+    const getHundredDigitThird = digitNames[number.toString()[2]];
     if (number < 20) {
         return digitNames[number];
     } else if (number >= 20 && number < 100) {
-        return number.toString().endsWith('0') ? `${tenDigitNames[number.toString()[0] - 2]}` : `${tenDigitNames[number.toString()[0] - 2]} ${digitNames[number.toString()[1]]}`;
+        return isLastZero ? `${getTenDigit}` : `${getTenDigit} ${getTenDigitSecond}`;
     } else if (number >= 100 && number < 1000) {
-        let word = `${digitNames[number.toString()[0]]} hundred`;
+        let word = `${getHundredDigit} hundred`;
 
-        if (number.toString()[1] === '0' && number.toString().endsWith('0')) {
+        if (isSecondZero && isLastZero) {
             word += ``;
-        } else if (number.toString()[1] === '0') {
-            word += ` ${digitNames[number.toString()[2]]}`;
-        } else if (number.toString().endsWith('0')) {
-            if (number.toString()[1] === '1') {
-                word += ` ${digitNames[number.toString()[1] + number.toString()[2]]}`;
+        } else if (isSecondZero) {
+            word += ` ${getHundredDigitThird}`;
+        } else if (isLastZero) {
+            if (isSecondOne) {
+                word += ` ${getHundredDigitTens}`;
             } else {
-                word += ` ${tenDigitNames[number.toString()[1] - 2]}`;
+                word += ` ${getHundredDigitTensRound}`;
             }
         } else {
-            if (number.toString()[1] === '1') {
-                word += ` ${digitNames[number.toString()[1] + number.toString()[2]]}`;
+            if (isSecondOne) {
+                word += ` ${getHundredDigitTens}`;
             } else {
-                word += ` ${tenDigitNames[number.toString()[1] - 2]} ${digitNames[number.toString()[2]]}`;
+                word += ` ${getHundredDigitTensRound} ${getHundredDigitThird}`;
             }
         }
 
