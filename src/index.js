72e6ae8471e1e9da012a337612ddf5401e170dc5
dench1k <denmoskvitin@gmail.com>
2020-10-22 12:00:19 +0300

    feat: add edge cases

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
     const digitNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const tenDigitNames = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
@@ -9,14 +9,22 @@ module.exports = function toReadable (number) {
     } else if (number >= 100 && number < 1000) {
         let word = `${digitNames[number.toString()[0]]} hundred`;
 
-        if (number.toString()[1] === '0') {
+        if (number.toString()[1] === '0' && number.toString().endsWith('0')) {
+            word += ``;
+        } else if (number.toString()[1] === '0') {
             word += ` ${digitNames[number.toString()[2]]}`;
         } else if (number.toString().endsWith('0')) {
-            word += ` ${tenDigitNames[number.toString()[1] - 2]}`;
-        } else if (number.toString()[1] === '1' ) {
-            word += ` ${digitNames[number.toString()[1]+number.toString()[2]]}`;
+            if (number.toString()[1] === '1') {
+                word += ` ${digitNames[number.toString()[1] + number.toString()[2]]}`;
+            } else {
+                word += ` ${tenDigitNames[number.toString()[1] - 2]}`;
+            }
         } else {
-            word += ` ${tenDigitNames[number.toString()[1] - 2]} ${digitNames[number.toString()[2]]}`;
+            if (number.toString()[1] === '1') {
+                word += ` ${digitNames[number.toString()[1] + number.toString()[2]]}`;
+            } else {
+                word += ` ${tenDigitNames[number.toString()[1] - 2]} ${digitNames[number.toString()[2]]}`;
+            }
         }
 
         return word;
